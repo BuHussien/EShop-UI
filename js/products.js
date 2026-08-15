8e89d90b82d6d41238f18de2ef78c124a95f94b4
ahmed abuhussien <buhussien2@gmail.com>
2026-08-15 20:04:51 +0300

    feat: add products data source (products + newArrivalProducts)

---
+++ js/products.js
@@ -0,0 +1,132 @@
+const products = [
+  {
+    id: "p1",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f1.jpg",
+    stars: 5,
+    cost: 44
+  },
+  {
+    id: "p2",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f2.jpg",
+    stars: 5,
+    cost: 99
+  },
+  {
+    id: "p3",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f3.jpg",
+    stars: 5,
+    cost: 79
+  },
+  {
+    id: "p4",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f4.jpg",
+    stars: 5,
+    cost: 99
+  },
+  {
+    id: "p5",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f5.jpg",
+    stars: 5,
+    cost: 55
+  },
+  {
+    id: "p6",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f6.jpg",
+    stars: 5,
+    cost: 24
+  },
+  {
+    id: "p7",
+    brand: "adidas",
+    name: "Cartoon Astronaut Pants",
+    image: "/img/products/f7.jpg",
+    stars: 5,
+    cost: 66
+  },
+  {
+    id: "p8",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/f8.jpg",
+    stars: 5,
+    cost: 34
+  }
+];
+const newArrivalProducts = [
+  {
+    id: "n1",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n1.jpg",
+    stars: 5,
+    cost: 44
+  },
+  {
+    id: "n2",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n2.jpg",
+    stars: 5,
+    cost: 99
+  },
+  {
+    id: "n3",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n3.jpg",
+    stars: 5,
+    cost: 79
+  },
+  {
+    id: "n4",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n4.jpg",
+    stars: 5,
+    cost: 99
+  },
+  {
+    id: "n5",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n5.jpg",
+    stars: 5,
+    cost: 55
+  },
+  {
+    id: "n6",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n6.jpg",
+    stars: 5,
+    cost: 24
+  },
+  {
+    id: "n7",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n7.jpg",
+    stars: 5,
+    cost: 66
+  },
+  {
+    id: "n8",
+    brand: "adidas",
+    name: "Cartoon Astronaut T-Shirts",
+    image: "/img/products/n8.jpg",
+    stars: 5,
+    cost: 34
+  }
+];
